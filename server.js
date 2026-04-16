9281b1490c8b735da8d3569b456f24c28fcb5143
ayana0409 <duongdoanthuan2003@gmail.com>
2026-04-16 09:29:20 +0700

    refactor: update server static file serving and adjust homepage configuration

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require('path');
 const app = express();
 const buildPath = path.join(__dirname, 'build');
 
-app.use(express.static(buildPath));
+app.use(express.static(buildPath, { index: false }));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(buildPath, 'index.html'));
 });
 
